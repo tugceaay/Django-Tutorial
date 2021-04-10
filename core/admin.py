cb707d3a21bf19db85e136bd134d5834d51042e7
tugceaay <tugceaay@gmail.com>
2021-04-10 19:26:02 +0300

    Models added and registered to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import SchoolModel, StudentModel, ClassModel
+
+
 
 # Register your models here.
+
+admin.site.register(SchoolModel)
+admin.site.register(StudentModel)
+admin.site.register(ClassModel)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+GRADE_CHOICES = (("PRI","İlkokul"),("MID","Ortaokul"),("HIGH","Lise"),("UNI","Üniversite"))
+class SchoolModel(models.Model):
+    #Okulun ismi, adresi, türü
+    name = models.CharField(max_length=100, default="İsimsiz Okul")
+    address = models.CharField(max_length=400, default="Adres yok")
+    grade = models.CharField(max_length=4, default="PRI",choices=GRADE_CHOICES)
 
-# Create your models here.
+    def __str__(self):
+        return "[" + self.grade + "]" + self.name + " - " + self.address
+
+class ClassModel(models.Model):
+    name = models.CharField(max_length=10, default="Belirsiz")
+    school = models.ForeignKey(SchoolModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.school.name + " - " + self.name
+
+class StudentModel(models.Model):
+    #isim, numara
+    name = models.CharField(max_length=100, default="İsimsiz")
+    number = models.CharField(max_length=10, default="Numarasız")
+    classname = models.ForeignKey(ClassModel, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.classname.school.name.upper() + ": " + self.classname.name + " - " + self.name + " ({})".format(self.number)
+
+
